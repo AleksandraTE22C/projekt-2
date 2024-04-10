510e3c7aa962de210cf7bd38add69288bcb6b8a1
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2024-04-10 11:06:40 +0200

    Började skriva på programmet

--- bokningssystem.java
+++ bokningssystem.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class bokningssystem{
     public static void main(String[] args) {
-        
+        Scanner tb = new Scanner (System.in);
+        int[] bokning = {};
+        System.out.println("1. Boka plats");
+        System.out.println("2. Skriv ut vilka platser är lediga");
+        System.out.println("3. Vinsten av sålda biljetter");
+        System.out.println("4. Avsluta");
+        int val= tb.nextInt();
+        if(val == 1){
+
+        }
+        else if(val == 2){
+
+        }
+        else if(val == 3){
+
+        }
+        else if(val == 4){
+            
+        }
+        else{
+            System.out.println("Välj ett giltigt nummer.");
+        }
     }
 }
